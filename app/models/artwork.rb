c9aed94984397130a5554e9b10f85f36f9e2ff42
Alap Dhruva <alap.dhruva2@gmail.com>
2024-02-19 19:01:11 -0500

    added styling to the view pages

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,11 +2,7 @@ class Artwork
   include HTTParty
 
   base_uri 'https://api.artic.edu'
-  default_params fields: 'id,api_link,title,image_id,artist_display,date_display,main_reference_number,description,thumbnail'
-
-  # def self.all
-  #   get '/api/v1/artworks?limit=100'
-  # end
+  default_params fields: 'id,api_link,title,image_id,artist_display,date_display,main_reference_number,description,thumbnail,place_of_origin'
 
   def self.searchArtwork(search)
     get "/api/v1/artworks/search?q=#{search}&limit=100"
